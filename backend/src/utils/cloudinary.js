8705c5f9e548bcbc25545d8867c018ba2eebd2df
mohit <mohitghegadmal2004@gmail.com>
2026-05-07 08:50:23 +0530

    Creating CLoudinary functionality

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({
+    cloud_name: process.env.ClOUDINARY_CLOUD_NAME,
+    api_key: process.env.ClOUDINARY_API_KEY,
+    api_secret: process.env.ClOUDINARY_API_SECRET
+})
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return  null
+
+        //Upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        console.log("File is uploaded on cloudinary", response.url)
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)  //revmoce the locally saved temp file as the uploaded opt get failed
+    }
+
+    return null;
+}
+
+cloudinary.v2.uploader.upload("",
+    {
+        public_id: ""
+    },
+    function(error, result) {
+        console.log(result);
+    }
+)
