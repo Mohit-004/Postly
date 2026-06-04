64b1daeb28fe82459a10e83d6090489bc47b9b9d
mohit <mohitghegadmal2004@gmail.com>
2026-06-04 20:45:19 +0530

    Try to access data in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await  fetch("http://localhost:8000/api/v1/users")
+
+        const data = await response.json();
+
+        setUsers(data);
+      } catch(error) {
+        console.log(error);
+      }
+
+    };
+
+    getUsers();
+   
+  }, []);
 
   return (
     <>
-      <div>
-        <h4>Hello Welcome To Postly</h4>
-      </div>
+    <div>
+      <h1>Users List</h1>
+
+      {users.map((user) => {
+        <p key={user.id}>{user.name}</p>
+      })}
+    </div>
+      
     </>
   )
 }
